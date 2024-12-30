e38205ad9e0af202f4e419dcf41b22dab680cfe7
felippeximenes <felippelpximenes@outlook.com>
2024-12-30 16:58:05 -0300

    adilção da edição dos usuarios

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ const prisma = new PrismaClient()
 const app  = express()
 app.use(express.json())
 
-const users = []
 
-app.post('/usuarios', async (req, res) => {
+app.put('/usuarios/:id', async (req, res) => {
 
-    await prisma.user.create({
-        data: {
-            email: req.body.email,
-            name: req.body.name,
+    console.log(req)
+
+     await prisma.user.update({
+         where: {
+             id: req.params.id
+         },
+
+         data: {
+             email: req.body.email,
+             name: req.body.name,
             age: req.body.age
-        }
-    })
-    
-    users.push(req.body)
+     }
+   })
 
     res.status(201).json(req.body)
 
@@ -30,6 +33,22 @@ app.get('/usuarios', async (req, res) => {
 
     res.status(200).json(users)
 })
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    
+
+    res.status(201).json(req.body)
+
+})
+
 
 
 app.listen(3000)
